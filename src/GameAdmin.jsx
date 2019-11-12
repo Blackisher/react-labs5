567e8749ccfd0287e3cbaab5a6d1e48a9d230ce0
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-11-12 22:55:22 +0100

    LAB5Task4 - "‘Played number of times’ should indicate how many times each user has played so far. "

--- src/GameAdmin.jsx
+++ src/GameAdmin.jsx
@@ -9,6 +9,7 @@ class GameAdmin extends Component{
         super(props);
         this.state = { user1Name: 'Jan',
                         user2Name: 'Anna',
+                        numberOfTimesPlayerPlayed: [1,0],
                         activePlayer: [1,0]} //1 user is playing, 0 user is not playing
         this.player1NameHandler = this.player1NameHandler.bind(this);
         this.player2NameHandler = this.player2NameHandler.bind(this);
@@ -31,10 +32,20 @@ class GameAdmin extends Component{
         var whoClicked = e.target.name;
         if (whoClicked === 'One') {
             this.setState((prevState, props) => ({
+                numberOfTimesPlayerPlayed:
+                    [
+                        prevState.numberOfTimesPlayerPlayed[0] + 1,
+                        prevState.numberOfTimesPlayerPlayed[1]
+                    ],
                 activePlayer: [1,0]
             }))
         } else if (whoClicked === 'Two') {
             this.setState((prevState, props) => ({
+                numberOfTimesPlayerPlayed:
+                    [
+                        prevState.numberOfTimesPlayerPlayed[0],
+                        prevState.numberOfTimesPlayerPlayed[1] + 1
+                    ],
                 activePlayer: [0,1]
             }))
         }
@@ -45,8 +56,8 @@ class GameAdmin extends Component{
         return (
             <div style={{ backgroundColor:'green' }}>
                 I’m Admin component
-                <Player playerNumber='One' playerName={this.state.user1Name} playerStatus={this.state.activePlayer[0]} playedNumberOftimes='5' onClickButtonHandler={this.playerClickButtonHandler}/>
-                <Player playerNumber='Two' playerName={this.state.user2Name} playerStatus={this.state.activePlayer[1]} playedNumberOftimes='6' onClickButtonHandler={this.playerClickButtonHandler}/>
+                <Player playerNumber='One' playerName={this.state.user1Name} playerStatus={this.state.activePlayer[0]} playedNumberOftimes={this.state.numberOfTimesPlayerPlayed[0]} onClickButtonHandler={this.playerClickButtonHandler}/>
+                <Player playerNumber='Two' playerName={this.state.user2Name} playerStatus={this.state.activePlayer[1]} playedNumberOftimes={this.state.numberOfTimesPlayerPlayed[1]} onClickButtonHandler={this.playerClickButtonHandler}/>
                 <hr/>
                 <InputPanel playerOneName={this.state.user1Name}  playerOneNameHandler={this.player1NameHandler}
                             playerTwoName={this.state.user2Name} playerTwoNameHandler={this.player2NameHandler}
