658d8a95f25ffec01c2d56447d8a6aa3a33b03a0
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-11-12 22:00:20 +0100

    LAB5Task2 - not important - code clean.

--- src/Player.jsx
+++ src/Player.jsx
@@ -4,7 +4,7 @@ function Player(props) {
     return (
         <div style={{ backgroundColor:'yellow', borderWidth:1, border:'solid',  width: 600}}>
             Player {props.playerNumber} component <br/>
-            Name {props.playerName} component <br/>
+            Name: {props.playerName} <br/>
             Played number of times: {props.playedNumberOftimes} <br/>
             <button>
                 {props.buttonText}
